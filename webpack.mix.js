8ed9d05ef146c58487b3300ebcea68f73b5198c2
alexey-khomenko <alexey.alexeevich.khomenko@gmail.com>
2021-04-23 17:10:12 +0300

    pug plugin

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,28 +1,50 @@
 let mix = require('laravel-mix');
 mix.pug = require('laravel-mix-pug-recursive');
+require('laravel-mix-imagemin');
 
 mix
     .setPublicPath('docs')
     .setResourceRoot('../')
-    .copyDirectory('resources/images/*.*', 'docs/images')
+    //.copyDirectory('resources/images/*.*', 'docs/images')
     .copyDirectory('resources/favicons/*.*', 'docs/favicons')
     .js('resources/scripts/app.js', 'js/scripts.js')
     .sass('resources/styles/app.scss', 'css/styles.css')
     .options({
         postCss: [
             require('tailwindcss')('tailwind.config.js'),
-        ]
+        ],
     })
     .pug('resources/views/pages/*.pug', 'docs', {
         excludePath: 'resources/views/pages',
         pug: {
-            pretty: true
-        }
+            pretty: true,
+        },
     })
 ;
 
 if (mix.inProduction()) {
+    mix
+        .imagemin(
+            'images/**.*',
+            {
+                context: 'resources',
+            },
+            {
+                optipng: {
+                    optimizationLevel: 5,
+                },
+                jpegtran: null,
+                plugins: [
+                    require('imagemin-mozjpeg')({
+                        quality: 100,
+                        progressive: true,
+                    }),
+                ],
+            },
+        );
     // todo сжать картинки
+    // todo фавиконки на асгаре проверить
+    // todo конфиги
 
     // const fs = require('fs');
     // const configs = [
@@ -35,9 +57,10 @@ if (mix.inProduction()) {
     //         mix.version(value);
     //     });
     // });
-} else {
+}
+else {
     mix.browserSync({
-        server: "./docs",
+        server: './docs',
         files: [
             'resources/**/*.*',
         ],
