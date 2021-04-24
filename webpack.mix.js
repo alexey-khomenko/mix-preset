404d51bc18d76ce3915ed08fcb7c823f53438ab0
alexey-khomenko <alexey.alexeevich.khomenko@gmail.com>
2021-04-24 17:27:59 +0300

    update

--- webpack.mix.js
+++ webpack.mix.js
@@ -5,8 +5,6 @@ require('laravel-mix-imagemin');
 mix
     .setPublicPath('docs')
     .setResourceRoot('../')
-    //.copyDirectory('resources/images/*.*', 'docs/images')
-    .copyDirectory('resources/favicons/*.*', 'docs/favicons')
     .js('resources/scripts/app.js', 'js/scripts.js')
     .sass('resources/styles/app.scss', 'css/styles.css')
     .options({
@@ -23,46 +21,37 @@ mix
 ;
 
 if (mix.inProduction()) {
-    mix
-        .imagemin(
+    mix.imagemin(
+        [
             'images/**.*',
-            {
-                context: 'resources',
+            'favicons/**.*',
+        ],
+        {
+            context: 'resources',
+        },
+        {
+            optipng: {
+                optimizationLevel: 5,
             },
-            {
-                optipng: {
-                    optimizationLevel: 5,
-                },
-                jpegtran: null,
-                plugins: [
-                    require('imagemin-mozjpeg')({
-                        quality: 100,
-                        progressive: true,
-                    }),
-                ],
-            },
-        );
-    // todo сжать картинки
-    // todo фавиконки на асгаре проверить
-    // todo конфиги
-
-    // const fs = require('fs');
-    // const configs = [
-    //     'resources/configs/favicons.json',
-    //     'resources/configs/images.json',
-    // ];
-    //
-    // configs.forEach(function (value) {
-    //     JSON.parse(fs.readFileSync(value).toString()).forEach(function (value) {
-    //         mix.version(value);
-    //     });
-    // });
+            jpegtran: null,
+            plugins: [
+                require('imagemin-mozjpeg')({
+                    quality: 100,
+                    progressive: true,
+                }),
+            ],
+        },
+    );
 }
 else {
-    mix.browserSync({
-        server: './docs',
-        files: [
-            'resources/**/*.*',
-        ],
-    });
+    mix
+        .copyDirectory('resources/images/*.*', 'docs/images')
+        .copyDirectory('resources/favicons/*.*', 'docs/favicons')
+        .browserSync({
+            server: './docs',
+            files: [
+                'resources/**/*.*',
+            ],
+        })
+    ;
 }
